1e291093f20461ceb723d9d224aade8c4001ddb7
Otumiky <otumiky@gmail.com>
2025-01-01 00:03:49 +0100

    updated

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "us-east-2"  # Change to your desired region 
+  region  = "us-east-1"  # Change to your desired region 
 }
 resource "aws_instance" "app" {
   ami           = var.ami_id
