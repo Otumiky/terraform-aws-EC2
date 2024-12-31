983bfeaf38efeb1ca0e2bc337fbf69701079ec71
Otumiky <otumiky@gmail.com>
2024-12-31 22:33:57 +0100

    updated variables

--- variable.tf
+++ variable.tf
@@ -14,14 +14,14 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
-#variable "aws_access_key_id" {
- # description = "AWS access key"
- # type        = string
- # sensitive   = true
-#}
+variable "aws_access_key_id" {
+  description = "AWS access key"
+  type        = string
+  sensitive   = true
+}
 
-#variable "aws_secret_access_key" {
- # description = "AWS secret key"
-#  type        = string
- # sensitive   = true
-#}
+variable "aws_secret_access_key" {
+  description = "AWS secret key"
+  type        = string
+  sensitive   = true
+}
